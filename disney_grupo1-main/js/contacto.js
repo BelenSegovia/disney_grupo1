9d8f675b2c330003b17fd10fce81622edc3cd619
FacundoVecino <facundovecino32@gmail.com>
2023-09-19 16:41:08 -0300

    fix(main) Juntando lo que va de proyecto

--- disney_grupo1-main/js/contacto.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-export class Contacto {
-    constructor (nombre, notas){
-        this.nombre = nombre;
-        this.notas = notas;
-        this.codigo = self.crypto.randomUUID();
-    };
-}; 
