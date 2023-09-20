cae9a35a36a78466ec2d81bd1e91100cdb2f3cd4
FacundoVecino <facundovecino32@gmail.com>
2023-09-20 19:04:38 -0300

    fix(index) Prueba de modal

--- javascript/index/app.js
+++ javascript/index/app.js
@@ -1,3 +1,11 @@
 import { cargarTabla } from "./indexUtils.js";
 
 window.addEventListener('load', cargarTabla);
+
+const cards = document.querySelectorAll('.card-disney');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    console.log('Hola en la consola');
+  });
+});
