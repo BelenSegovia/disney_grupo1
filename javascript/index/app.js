83cfc6f202b8b99f7c3672a68386cf92ade77c41
Enzo Iñigo <enzoinigo96@gmail.com>
2023-09-20 23:09:16 -0300

    agrego filtro de busqueda

--- javascript/index/app.js
+++ javascript/index/app.js
@@ -9,3 +9,23 @@ cards.forEach(card => {
     console.log('Hola en la consola');
   });
 });
+
+// BUSCADOR 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buscador = document.getElementById('buscador');
+
+    buscador.addEventListener('keyup', () => {
+        const searchTerm = buscador.value.toLowerCase();
+        const cards = document.querySelectorAll('.card-disney');
+
+        cards.forEach(card => {
+            const name = card.getAttribute('name').toLowerCase();
+            if (name.includes(searchTerm)) {
+                card.style.display = 'block'; // Mostrar tarjeta si coincide
+            } else {
+                card.style.display = 'none'; // Ocultar tarjeta si no coincide
+            }
+        });
+    });
+});
